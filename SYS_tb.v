8be35b5a3f96b390ab7a46430d1f061c64a70e11
cho minjun <choalswns99@gmail.com>
2026-05-20 12:20:36 +0900

    Add files via upload

---
+++ SYS_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/05/07 22:26:44
+// Design Name: 
+// Module Name: SYS_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SYS_tb(
+
+    );
+endmodule
